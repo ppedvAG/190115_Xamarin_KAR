a0ece714d854ef304d89f0d0d570b5e938784880
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-15 12:19:44 +0100

    MessageBox

--- HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
+++ HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
@@ -13,5 +13,25 @@ namespace HalloXForms
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // Klassische "MessageBox"
+            // DisplayAlert("Meine erste Nachricht", "Hallo Welt", "OK");
+
+            // "Ja/Nein - Messagebox"
+            // DisplayAlert("Eine wichtige Frage", "Mittagspause ?", "Ja", "Verdammt nochmal, JA !");
+
+            // Mehrere möglichkeiten als Dialog -  aka "Combobox-Dialog"
+            // DisplayActionSheet("Pseudo-Combobox", "Ok", "Abbrechen", new string[] { "Apfel", "Birne", "Banane" });
+
+            // async/await: UI-Thread kann weitermachen während gewartet
+            string auswahl = await DisplayActionSheet("Pseudo-Combobox", "Ok", "Abbrechen", new string[] { "Apfel", "Birne", "Banane" });
+            await DisplayAlert("Deine Auwahl war:", auswahl, "Yay !");
+
+            // Synchron: -> Auf Task.Result warten (funktioniert wie Thread.Join(), blockiert das UI)
+            // string auswahl2 =  DisplayActionSheet("Pseudo-Combobox", "Ok", "Abbrechen", new string[] { "Apfel", "Birne", "Banane" }).Result;
+
+        }
     }
 }
