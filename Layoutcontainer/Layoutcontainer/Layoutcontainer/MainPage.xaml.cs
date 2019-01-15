3af0e30f1b0119cb761341c8f8269e2024331e1d
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-15 16:19:08 +0100

    Stacklayout, Grid und Styles

--- Layoutcontainer/Layoutcontainer/Layoutcontainer/MainPage.xaml.cs
+++ Layoutcontainer/Layoutcontainer/Layoutcontainer/MainPage.xaml.cs
@@ -13,5 +13,14 @@ namespace Layoutcontainer
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Style = (Style)this.Resources["blueButtonStyle"];
+
+            // Unterschied zwischen StaticResource und DynamicResource:
+            // Static:  Wertänderungen werden erkannt, XAML-Parser nimmt die Ressource sofort
+            // Dynamic: Referenzänderungen werden erkannt (zb neues Objekt), XAML-Parser löst es nach beenden des Parsens auf
+        }
     }
 }
