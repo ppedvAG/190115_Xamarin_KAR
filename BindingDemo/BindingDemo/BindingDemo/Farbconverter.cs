a10a5d69fa517b90b0825cfcbcf0cb35bfbbb278
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-16 14:58:00 +0100

    Profivariante für Farben

--- BindingDemo/BindingDemo/BindingDemo/Farbconverter.cs
+++ BindingDemo/BindingDemo/BindingDemo/Farbconverter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Xamarin.Forms;
 
@@ -13,11 +15,23 @@ namespace BindingDemo
             // Color.FromHex()
 
             string farbe = (string)value;
-            if (farbe == "Rot")
-                return Color.Red;
-            else if (farbe == "Grün")
-                return Color.Green;
-            else
+            //if (farbe == "Rot")
+            //    return Color.Red;
+            //else if (farbe == "Grün")
+            //    return Color.Green;
+            //else
+            //    return Color.Black;
+
+            // Profi-Variante
+
+            FieldInfo[] allfields = typeof(Color).GetFields();
+
+            if (allfields.Any(x => x.Name.ToLower() == farbe.ToLower()))
+            {
+                return allfields.First(x => x.Name.ToLower() == farbe.ToLower())
+                                .GetValue(typeof(Color));
+            }
+            else // Farbe nicht vorhanden
                 return Color.Black;
         }
 
