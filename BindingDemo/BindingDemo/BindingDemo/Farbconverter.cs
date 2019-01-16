6494352a2e46f1b8f80c579553568fb64c380c2c
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-16 13:52:21 +0100

    Init LINQ - Demo

--- BindingDemo/BindingDemo/BindingDemo/Farbconverter.cs
+++ BindingDemo/BindingDemo/BindingDemo/Farbconverter.cs
@@ -10,6 +10,8 @@ namespace BindingDemo
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // Color.FromHex()
+
             string farbe = (string)value;
             if (farbe == "Rot")
                 return Color.Red;
