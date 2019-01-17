a3442eb411ea7f4f65a5e26e9d5a95fca104b0a9
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-17 15:24:33 +0100

    MVVM_simpel

---
+++ MVVM_Simpel/MVVM_Simpel/MVVM_Simpel/Services/PersonenService.cs
@@ -0,0 +1,27 @@
+﻿using MVVM_Simpel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_Simpel.Services
+{
+    public class PersonenService
+    {
+        public List<Person> GetPersonen()
+        {
+            return new List<Person>
+            {
+                new Person{Vorname="Tom",Nachname="Ate",Alter=10,Kontostand=100m,Telefonnummer="12345"},
+                new Person{Vorname="Anna",Nachname="Nass",Alter=20,Kontostand=200m,Telefonnummer="12345"},
+                new Person{Vorname="Peter",Nachname="Silie",Alter=30,Kontostand=3000m,Telefonnummer="12345"},
+                new Person{Vorname="Franz",Nachname="Ose",Alter=40,Kontostand=-4000m,Telefonnummer="12345"},
+                new Person{Vorname="Martha",Nachname="Pfahl",Alter=50,Kontostand=500000m,Telefonnummer="12345"},
+                new Person{Vorname="Klara",Nachname="Fall",Alter=60,Kontostand=66666m,Telefonnummer="12345"},
+                new Person{Vorname="Rainer",Nachname="Zufall",Alter=70,Kontostand=-10000m,Telefonnummer="12345"},
+                new Person{Vorname="Frank N",Nachname="Stein",Alter=80,Kontostand=88888m,Telefonnummer="12345"},
+                new Person{Vorname="Anna",Nachname="Bolika",Alter=90,Kontostand=-12345m,Telefonnummer="12345"},
+                new Person{Vorname="Axel",Nachname="Schweiß",Alter=100,Kontostand=100000000m,Telefonnummer="12345"}
+            };
+        }
+    }
+}
---
+++ MVVM_Simpel/MVVM_Simpel/MVVM_Simpel/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MVVM_Simpel.Models;
+using MVVM_Simpel.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_Simpel.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            // Kontrollfreak-Antipattern
+            service = new PersonenService();
+            GetPersonenCommand = new Command(GetPersonen);
+        }
+
+        private readonly PersonenService service;
+
+        private List<Person> personenliste;
+        public List<Person> Personenliste
+        {
+            // Seit C# 7.0
+            get => personenliste;
+            set => SetProperty(ref personenliste, value);
+        }
+        public Command GetPersonenCommand { get; set; }
+        private void GetPersonen(object obj)
+        {
+            Personenliste = service.GetPersonen();
+            // ObservableCollection<T> -> Add(), Remove(), Clear()
+            // BindingList<T> -> Schaut auch auf die Properties der Elemente IN der Liste (z.B. Vorname ändern = Aktualisierung)
+        }
+    }
+}
