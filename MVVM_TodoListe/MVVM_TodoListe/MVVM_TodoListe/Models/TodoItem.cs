a920d817c55ba5c9764f6db13cc84a107c80ce1b
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-17 16:08:03 +0100

    TodoListe - Service erstellt Übung: Vervollständigen ;)

---
+++ MVVM_TodoListe/MVVM_TodoListe/MVVM_TodoListe/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_TodoListe.Models
+{
+    public class TodoItem
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+    }
+}
---
+++ MVVM_TodoListe/MVVM_TodoListe/MVVM_TodoListe/Services/TodoItemService.cs
@@ -0,0 +1,27 @@
+﻿using MVVM_TodoListe.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MVVM_TodoListe.Services
+{
+    public class TodoItemService
+    {
+        public List<TodoItem> GetTodoItems()
+        {
+            // 1) json runterladen
+            // 2) deserialisieren
+            // 3) todoitem-liste zurückgeben
+
+            string json;
+            using (HttpClient client = new HttpClient())
+            {
+                json = client.GetStringAsync("https://jsonplaceholder.typicode.com/todos").Result;
+            }
+
+            return JsonConvert.DeserializeObject<List<TodoItem>>(json);
+        }
+    }
+}
