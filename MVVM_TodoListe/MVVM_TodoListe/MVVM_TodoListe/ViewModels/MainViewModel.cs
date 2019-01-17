1edd9aa39fd3deaad9dd968f5e6330156143fef4
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-17 17:04:29 +0100

    Übung fertig

---
+++ MVVM_TodoListe/MVVM_TodoListe/MVVM_TodoListe/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MVVM_TodoListe.Models;
+using MVVM_TodoListe.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MVVM_TodoListe.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private readonly TodoItemService service;
+
+        private List<TodoItem> todoitemliste;
+
+        public List<TodoItem> ToDoItemListe
+        {
+            get => todoitemliste;
+            set => SetProperty(ref todoitemliste, value);
+        }
+
+        public Command GetToDoCommand { get; set; }
+
+        public MainViewModel()
+        {
+            // Kontrollfreak-Antipattern
+
+            service = new TodoItemService();
+            GetToDoCommand = new Command(GetToDoItems);
+        }
+
+        private void GetToDoItems(object obj)
+        {
+            ToDoItemListe = service.GetTodoItems();
+        }
+    }
+}
