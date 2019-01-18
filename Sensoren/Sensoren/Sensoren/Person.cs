db135cc0ec598535315cfd18e37d3d95a3f9a0bc
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-18 14:42:39 +0100

    SQLite

---
+++ Sensoren/Sensoren/Sensoren/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace Sensoren
+{
+    [Table("Personentabelle")]
+    class Person
+    {
+        [PrimaryKey,AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(255)]
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        // [Ignore]
+        public decimal Kontostand { get; set; }
+    }
+
+}
---
+++ Sensoren/Sensoren/Sensoren/SQLitePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sensoren
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SQLitePage : ContentPage
+	{
+		public SQLitePage ()
+		{
+			InitializeComponent ();
+            string fullPath = Path.Combine(FileSystem.AppDataDirectory, "db.sqlite");
+
+            con = new SQLiteConnection(fullPath); // Wenn die DB noch nicht existiert, wird sie erstellt. Wenn sie exisitert, wird sie geöffnet
+            con.CreateTable<Person>(); // Wenn die Tabelle bereits da ist, passiert NICHTS
+            con.s
+        }
+
+        private readonly SQLiteConnection con;
+
+
+        private void ButtonEinfügen_Clicked(object sender, EventArgs e)
+        {
+            Person p = new Person
+            {
+                Vorname = entryName.Text,
+                Nachname = "Mustermann",
+                Alter = 10,
+                Kontostand = 100
+            };
+
+            con.Insert(p);
+        }
+
+        private void ButtonLaden_Clicked(object sender, EventArgs e)
+        {
+            listViewPersonen.ItemsSource = con.Table<Person>().ToList();
+        }
+
+        private void ListViewPersonen_Refreshing(object sender, EventArgs e)
+        {
+            listViewPersonen.ItemsSource = con.Table<Person>().ToList();
+            listViewPersonen.EndRefresh();
+        }
+    }
+}
